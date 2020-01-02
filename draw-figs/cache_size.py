50e12ce502ba5e70d12cdf1ad82a3054c7c2154d
kanyuanzhi <kan@mail.ustc.edu.cn>
2020-01-02 20:20:26 +0800

    init

--- draw-figs/cache_size.py
+++ draw-figs/cache_size.py
@@ -1,5 +1,25 @@
 import sys
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    pass
+    cache_size = []
+    load_reactive = []
+    load_subpub = []
+    load_freshness = []
+    load_nocache = []
+
+    hitratio_reactive = []
+    hitratio_subpub = []
+    hitratio_freshness = []
+    hitratio_nocache = []
+    
+    f = open(r"draw-figs/cache_size.txt")
+    line = f.readline()
+        line = line.split(" ")
+        cache_size.append(int(line[1]))
+        seq = line[8][:-1]
+        name = line[14][:-1]
+
+        line = f.readline()
+    
+    f.close()
